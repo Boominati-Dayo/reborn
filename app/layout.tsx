d1cdf4a1b3890f4d8591db833fb5f5df5214e968
Boominati-Dayo <mishaelekom@gmail.com>
2026-06-07 13:12:12 -0400

    Fix Smartsupp - use dangerouslySetInnerHTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,18 +129,20 @@ export default function RootLayout({
           <Toaster position="bottom-right" />
           <OfflineIndicator />
         </CartProvider>
-        <Script id="smartsupp" strategy="afterInteractive">
-          {`
-            var _smartsupp = _smartsupp || {};
-            _smartsupp.key = '060a614999320f74ef75dae9b49aed8c250119ed';
-            window.smartsupp||(function(d) {
-              var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-              s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-              c.type='text/javascript';c.charset='utf-8';c.async=true;
-              c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-            })(document);
-          `}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _smartsupp = _smartsupp || {};
+              _smartsupp.key = '060a614999320f74ef75dae9b49aed8c250119ed';
+              window.smartsupp||(function(d) {
+                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+              })(document);
+            `,
+          }}
+        />
         <noscript>
           Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a>
         </noscript>
