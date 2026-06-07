7277c5e22ed49839b5299c39387aecb4df17face
Boominati-Dayo <mishaelekom@gmail.com>
2026-06-07 13:02:55 -0400

    Add Smartsupp live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { CartProvider } from "@/lib/context/cart-context";
 import { MainContent } from "@/components/layout/main-content";
 import { Toaster } from "react-hot-toast";
 import { OfflineIndicator } from "@/components/ui/offline-indicator";
+import Script from "next/script";
 import "./globals.css";
 
 const bodyFont = Inter({
@@ -128,6 +129,21 @@ export default function RootLayout({
           <Toaster position="bottom-right" />
           <OfflineIndicator />
         </CartProvider>
+        <Script id="smartsupp" strategy="afterInteractive">
+          {`
+            var _smartsupp = _smartsupp || {};
+            _smartsupp.key = '060a614999320f74ef75dae9b49aed8c250119ed';
+            window.smartsupp||(function(d) {
+              var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+              s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+              c.type='text/javascript';c.charset='utf-8';c.async=true;
+              c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+            })(document);
+          `}
+        </Script>
+        <noscript>
+          Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a>
+        </noscript>
       </body>
     </html>
   );
