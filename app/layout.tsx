d7fb9638ef0464c7444c80d0e3d80655164eb823
Boominati-Dayo <mishaelekom@gmail.com>
2026-07-27 08:39:02 -0400

    Replace Smartsupp with Tawk.to live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,7 +129,21 @@ export default function RootLayout({
           <Toaster position="bottom-right" />
           <OfflineIndicator />
         </CartProvider>
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a674f27bc55a11d4d710468/1juhoqdqu';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
