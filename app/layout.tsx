f5e51a56a33d7f6caf897f76dcc80e94f6765335
Boominati-Dayo <mishaelekom@gmail.com>
2026-07-29 19:44:33 -0400

    Remove Smartsupp live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -149,20 +149,6 @@ export default function RootLayout({
           <OfflineIndicator />
         </CartProvider>
         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _smartsupp = _smartsupp || {};
-              _smartsupp.key = '060a614999320f74ef75dae9b49aed8c250119ed';
-              window.smartsupp||(function(d) {
-                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-                c.type='text/javascript';c.charset='utf-8';c.async=true;
-                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-              })(document);
-            `,
-          }}
-        />
-        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
